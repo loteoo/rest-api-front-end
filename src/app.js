d2edcd67b15abd49b7670668f9b2782124691fb4
Alexandre Lotte <alexlotte16@gmail.com>
2019-03-21 23:52:07 -0400

    fix

--- src/app.js
+++ src/app.js
@@ -7,15 +7,9 @@ import {init} from './app/init'
 import {view} from './app/view'
 import {subscriptions} from './app/subscriptions'
 
-import {FetchUsers, ParseUrl} from './app/actions'
 import {enableOnMountDomEvent} from './utils'
 
 enableOnMountDomEvent()
 
 // Initialize the app
-app({
-  init: FetchUsers(ParseUrl(init, window.location.pathname.replace(baseHref, ''))),
-  view,
-  subscriptions,
-  container: document.body
-})
+app({init, view, subscriptions, container: document.body})
--- src/app/init.js
+++ src/app/init.js
@@ -1,7 +1,12 @@
-export const init = {
+import {baseHref} from '../utils'
+import {FetchUsers, ParseUrl} from './actions'
+
+const initialState = {
   userId: null,
   isFetching: true,
   fetched: undefined,
   users: [],
   userData: {}
 }
+
+export const init = FetchUsers(ParseUrl(initialState, window.location.pathname.replace(baseHref, '')))
